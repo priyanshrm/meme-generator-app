95bbfc8f273cb88fa917484cd36aae3604ee7ea4
priyanshrm <priyanshmahendra@gmail.com>
2023-04-03 12:40:06 +0530

    Add text to meme image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,17 +2,29 @@ import React from "react"
 
 export default function foo(props){
     const [meme, setMeme] = React.useState({
-        toptext: "",
-        bottomtext: "",
+        topText: "",
+        bottomText: "",
         randomImage: ""
     })
 
     const array = props.memes.data.memes
 
+    // console.log(meme.topText, meme.bottomText)
+
     function handleClick(e) {
         e.preventDefault()
         const num = Math.floor(Math.random() * array.length)
         setMeme(prevState => ({...prevState, randomImage: array[num].url}))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevState => {
+            return {
+                ...prevState, 
+                [name]:value
+            }
+        })
     }
 
     return (
@@ -22,11 +34,17 @@ export default function foo(props){
                     type="text"
                     className="form--input"
                     placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     className="form--input"
                     placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button
                     className="form--btn"
@@ -37,6 +55,8 @@ export default function foo(props){
             </form>
             <div className="form--img--container">
                 <img src={meme.randomImage} className="form--img"></img>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
